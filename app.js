d49c7d12309dbb51794def47dc649db9e255fc34
Xavier Graham <128425706+xcg2003@users.noreply.github.com>
2025-02-20 22:45:50 +0000

    Added all the pages in node.

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ app.get("/", (req, res) => {
     res.send("hello world");
 });
 
+app.get("/homepage", (req, res) => {
+    res.send("home page here, user can search through there database of books");
+})
+
+app.get("/bookMark", (req, res) => {
+    res.send("display users book marks here");
+})
+
+app.get("/bookSearch", (req, res) => {
+    res.send("user will search books on this page using googlebooks API");
+})
+
+
 app.listen(port, ()=> {
     console.log(`App listening on port ${port}`);
 })
