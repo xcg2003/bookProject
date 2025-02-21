024a294381a11a7f69634cbdfa94afe226b4abaf
Xavier Graham <128425706+xcg2003@users.noreply.github.com>
2025-02-21 23:18:39 +0000

    The home page will display the static main page.

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 import express from 'express';
 import { get } from 'http';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url); // get the name of the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+// set the view engine
+app.set('views', path.join(__dirname, 'views'));
+
+// define a static files directory (CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
 app.get("/homepage", (req, res) => {
     res.send("home page here, user can search through there database of books");
-})
+});
 
 app.get("/bookMark", (req, res) => {
     res.send("display users book marks here");
-})
+});
 
 app.get("/bookSearch", (req, res) => {
     res.send("user will search books on this page using googlebooks API");
-})
+});
 
 
 app.listen(port, ()=> {
     console.log(`App listening on port ${port}`);
-})
+});
