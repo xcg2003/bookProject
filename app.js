343cbcbcf3240cda0081ee64916087870d12c2b4
Xavier <128425706+xcg2003@users.noreply.github.com>
2025-02-23 14:49:49 -0700

    Switched to ejs file, added .env, added password checking.

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 import express from 'express';
-import { get } from 'http';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import dotenv  from 'dotenv'; // To use env variables
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url); // get the name of the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'pubic/CSS')));
+app.use(express.static(path.join(__dirname, 'public/CSS')));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/home.html'));
+    res.render("home");
+});
+
+app.post("/", (req,res) =>{
+    const data = req.body;
+    const password = process.env.HOME_PASSWORD;
+    const username = process.env.HOME_USERNAME;
+    if(username != data.username && username != data.password)
+    {
+        console.log("Incorrect Username and password");
+    }
+    else if(password != data.password){
+        console.log("Incorrect password");
+    }
+    else if(username != data.username){
+        console.log("Incorrect username");
+    }
+    else{
+        console.log("Correct Creds");
+    }
 });
 
 app.get("/homepage", (req, res) => {
