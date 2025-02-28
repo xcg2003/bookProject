579a41ddc975b85d83494de6a734a48cb9fffd62
Xavier <128425706+xcg2003@users.noreply.github.com>
2025-02-27 17:05:14 -0700

    format change

--- public/JavaScript/login.js
+++ public/JavaScript/login.js
@@ -1,19 +1,14 @@
 //checks if valid creds where givin, if they are not valid creds 
 //displays a message to the page that says incorrect creds
+
+
 function checkCredtials()
 {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     const insertHtmlDiv = document.getElementById("JavaScript");
 
-    if(username != process.env.HOME_USERNAME && password != process.env.HOME_PASSWORD){
-        insertHtmlDiv.innerHTML = "Incorrect Username or Password";
-        console.log("Incorrect Username or Password");
-        return;
-    }
-    else if(password != process.env.HOME_PASSWORD){
-        insertHtmlDiv.innerHTML = "Incorrect Password"
-        console.log("Incorrect Password");
-        return;
-    }
+   //need to add a fetch post request to the node backend in order to know if the username and password inputed are vaild.
+
+   
 }
